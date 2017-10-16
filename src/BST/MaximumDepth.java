37f400a2cd5dc1b99d12d62833186540740e8198
peter5500 <31715971+peter5500@users.noreply.github.com>
2017-10-16 14:25:00 -0700

    postorder

---
+++ src/BST/MaximumDepth.java
@@ -0,0 +1,35 @@
+package BST;
+
+//divide
+public class MaximumDepth {
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return Math.max(left,right)+1;
+    }
+}
+
+class MaximumDepth2 {
+    private int depth;
+
+    public int maxDepth(TreeNode root) {
+    depth = 0;
+    helper(root,1);
+    return depth;
+}
+private void helper(TreeNode node, int curdepth){
+        if (node == null){
+            return;
+        }
+        if (curdepth > depth){
+            depth = curdepth;
+        }
+
+        helper(node.left,curdepth+1);
+        helper(node.right,curdepth+1);
+    }
+}
