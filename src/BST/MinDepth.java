e78b9d8b244de99d94d6c3ee8c7c2f5f9777edd3
peter5500 <31715971+peter5500@users.noreply.github.com>
2017-10-16 15:56:22 -0700

    MinDepth

--- src/BST/MinDepth.java
+++ src/BST/MinDepth.java
@@ -13,3 +13,22 @@ public class MinDepth {
     }
 }
 
+
+class MinDepth2 {
+    public int minDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return getmin(root);
+    }
+
+    public int getmin(TreeNode root) {
+        if (root == null) {
+            return Integer.MAX_VALUE;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        return Math.min(getmin(root.left), getmin(root.right)) + 1;
+    }
+}
