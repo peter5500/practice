5f1f4083c7b6022ebc43f0a659f07c1a13695142
peter5500 <31715971+peter5500@users.noreply.github.com>
2017-10-18 19:55:46 -0700

    treePath

--- src/BST/treePath.java
+++ src/BST/treePath.java
@@ -3,12 +3,30 @@ package BST;
 import java.util.ArrayList;
 import java.util.List;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
 public class treePath {
     /*
      * @param root: the root of the binary tree
      * @return: all root-to-leaf paths
      */
-    public List<String> binaryTreePaths(TreeNode root) {
+           public static void main(String[] args) {
+               TreeNode root = new TreeNode(3);
+               TreeNode p1 = new TreeNode(1);
+               TreeNode p3 = new TreeNode(0);
+
+               root.left = p1;
+               p1.left = p3;
+
+               System.out.println(binaryTreePaths(root));
+           }
+            public static List<String> binaryTreePaths(TreeNode root) {
+
         List<String> paths = new ArrayList<>();
         if (root == null){
             return paths;
