672862f2ea003a042e2204f83423054a80a3b281
peter5500 <31715971+peter5500@users.noreply.github.com>
2017-10-19 19:23:02 -0700

    serialize

---
+++ src/DFS/Serialize.java
@@ -0,0 +1,40 @@
+package DFS;
+
+import java.util.ArrayList;
+
+public class Serialize {
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "{}";
+        }
+
+        ArrayList<TreeNode> list = new ArrayList<>();
+        list.add(root);
+
+        for (int i = 0; i < list.size(); i++) {
+            TreeNode node = list.get(i);
+            if (node == null) {
+                continue;
+            }
+            list.add(node.left);
+            list.add(node.right);
+        }
+        while (list.get(list.size() - 1) == null) {
+            list.remove(list.size() - 1);
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        sb.append(list.get(0).val);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) == null) {
+                sb.append(",#");
+            } else {
+                sb.append(",");
+                sb.append(list.get(i).val);
+
+            }
+        }
+        return sb.toString();
+    }
+}
+
